c31823d08c09a33f666cd939abd9ca24292d4971
Jimmy Defains <jimmydef@outlook.fr>
2024-04-10 16:40:18 +0200

    style: ajout css datePicker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -4,19 +4,13 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import "./datePicker.scss";
-import { colors } from "../sass/variables-colors-typo";
 
 const CustomDatePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
-
+  const [startDate, setStartDate] = useState(null);
+  const today = new Date();
   const MyContainer = ({ className, children }) => {
     return (
-      <div
-        style={{
-          padding: "10px",
-          background: colors.secondary,
-          color: "#fff",
-        }}>
+      <div className="pop-up-wrapper">
         <CalendarContainer className={className}>
           <div style={{ position: "relative" }}>{children}</div>
         </CalendarContainer>
@@ -27,8 +21,25 @@ const CustomDatePicker = () => {
     className: PropTypes.string,
     children: PropTypes.array,
   };
+  const dayClassName = (date) => {
+    if (
+      startDate &&
+      date.toDateString() === startDate.toDateString() &&
+      date.toDateString() === today.toDateString()
+    ) {
+      return "selected-and-today";
+    }
+    if (date.toDateString() === today.toDateString())
+      return "date-picker-today";
+
+    if (startDate && date.toDateString() === startDate.toDateString())
+      return "selected-day";
+  };
+
   return (
     <DatePicker
+      dayClassName={dayClassName}
+      showIcon
       className="custom-input"
       calendarContainer={MyContainer}
       selected={startDate}
